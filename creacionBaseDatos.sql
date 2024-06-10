0d8f8f22557dfacf0fd8e015116abc3b9c5d1f3c
Edgar Mila <edgarmila_10@outlook.com>
2024-06-10 07:40:37 +0200

    Avanzado en la memoria

--- creacionBaseDatos.sql
+++ creacionBaseDatos.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
     email_address VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    phonenumber CHAR(10) UNIQUE,
+    phonenumber CHAR(10),
     follower_count INT NOT NULL DEFAULT 0,
     following_count INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
