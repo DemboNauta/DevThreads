ef7b78880267191c90aea208c812d85daf4ae423
Edgar Milá <practicas4@sernivet.net>
2024-06-12 12:06:55 +0200

    Cambios en la conexión a la bbdd

--- bbdd.php
+++ bbdd.php
@@ -1,7 +1,5 @@
 <?php
 $mysqli = new mysqli("localhost", "u645142794_edgar", "Edgarana1", "u645142794_devthreads");
-
-$mysqli = new mysqli("localhost", "u645142794_edgar", "Edgarana1", "u645142794_devthreads");
     if ($mysqli->connect_errno) {
         header('Content-Type: application/json');
         echo json_encode(array("error" => "Error en la conexión a MySQL: " . $mysqli->connect_error));
